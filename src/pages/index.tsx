8824e69925f246ed7a3f8289d1e74d40db9a794b
Vlady Veselinov <vladkodmc@gmail.com>
2021-02-27 14:44:14 +0000

    update copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ import { Main } from '../templates/Main'
 const About = () => (
   <Main meta={<Meta title="Vyatr" description="Games" />}>
     <Content>
-      <section className="p-2 rounded mb-3">
+      <section className="p-2 rounded mb-3 bg-green-50">
         <h1>
           <b>🌳 Trees</b>
         </h1>
-        <p>Bring winter onto the world.</p>
+        <p>This is our first game! ❄You're a magic snowball that brings winter onto the world. If you'd like to play it, please subscribe to our newsletter below.</p>
       </section>
       <Subscribe />
     </Content>
--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
-  site_name: 'Vyatr',
-  title: 'Vyatr',
-  description: 'We make games in our free time, like the whisper of the wind 🍃',
+  site_name: '🍃Vyatr',
+  title: '🍃Vyatr',
+  description: 'We make games in our free time, like the whisper of the wind',
   url: 'https://vyatr.com',
   locale: 'en',
   author: 'The Vyatr Team',
